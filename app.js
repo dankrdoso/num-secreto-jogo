6f193ecd9a660173b0db629ee386004d697bda82
Daniel Cardoso <142063604+dankrdoso@users.noreply.github.com>
2024-06-16 14:25:39 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+let listaNumSorteados = [];
+let numSecreto = sorteioNumSecreto();
+let tentativas = 1;
+
+
+
+function exibirTextoTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Male', {rate:1.2});
+}   
+
+function exibirMsgInicial() {
+    exibirTextoTela('h1', 'JOGO DO NÚMERO SECRETO');
+    exibirTextoTela('p', 'Escolha um número entre 1 e 10.');
+}
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    if (chute == numSecreto){
+        exibirTextoTela('h1', 'ACERTOU!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+        let msgTentativas = `Você descobriu o número secreto em ${tentativas} ${palavraTentativa}`
+        exibirTextoTela('p', msgTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numSecreto) {
+            exibirTextoTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}    
+
+function sorteioNumSecreto() {
+    let numSecreto = parseInt(Math.random() * 10 +1);
+    if (listaNumSorteados.includes(numSecreto)) {
+        return sorteioNumSecreto();
+    } else {
+        listaNumSorteados.push(numSecreto)
+        return numSecreto
+    }
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numSecreto = sorteioNumSecreto();
+    limparCampo();
+    tentativas = 1;
+    exibirMsgInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true)
+}
+
+exibirMsgInicial();
